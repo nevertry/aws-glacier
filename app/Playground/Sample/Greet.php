4ecc12ffc9a75e469fb9e93897ea541221570d0c
Hendy Yanuar <hendy.yanuar@gmail.com>
2019-03-26 11:41:49 +0700

    Add Greet class to main

---
+++ app/Playground/Sample/Greet.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Playground\Sample;
+
+class Greet
+{
+    public function hello($str='World')
+    {
+        echo 'Hello '.$str;
+    }
+}
---
+++ main.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ .'/vendor/autoload.php';
+
+use App\Playground\Sample\Greet;
+
+$greet = new Greet();
+
+$greet->hello();
+
